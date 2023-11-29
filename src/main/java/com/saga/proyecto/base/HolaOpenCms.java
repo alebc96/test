447c3380cbc65ad658aafe9dd51aff76f9169c58
Manuel Alejandro Becerra Castro <alejandro.becerra@sagasoluciones.com>
2023-11-29 20:17:59 +0100

    first commit

--- src/main/java/com/saga/proyecto/base/HolaOpenCms.java
+++ src/main/java/com/saga/proyecto/base/HolaOpenCms.java
@@ -2,6 +2,6 @@ package com.saga.proyecto.base;
 
 public class HolaOpenCms {
     public static void main(String[] args) {
-        System.out.println("Hola OpenCms!");
+        System.out.println("Hola OpenCms! x2");
     }
 }
